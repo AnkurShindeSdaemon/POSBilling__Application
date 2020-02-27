e403b285e04642cb55a5cbc325f3287fac95b951
AnkurShindeSdaemon <ankur.shinde@sdaemon.com>
2020-02-27 18:50:26 +0530

    @27/02/2020 6.49pm Day off commit

--- app/src/main/java/com/posbilling/posbillingapplication/activity/dashboard/ui/addcustomer/FragmentAddCustomer.java
+++ app/src/main/java/com/posbilling/posbillingapplication/activity/dashboard/ui/addcustomer/FragmentAddCustomer.java
@@ -3,6 +3,7 @@ package com.posbilling.posbillingapplication.activity.dashboard.ui.addcustomer;
 import android.Manifest;
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -11,23 +12,34 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.text.Editable;
 import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.Window;
 import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
 import com.posbilling.posbillingapplication.R;
 import com.posbilling.posbillingapplication.activity.dashboard.ActivityDashboard;
+import com.posbilling.posbillingapplication.interfaceclick.OnStateListClick;
+import com.posbilling.posbillingapplication.model.StateModel;
 import com.posbilling.posbillingapplication.utility.BaseFragment;
 import com.posbilling.posbillingapplication.utility.BasePresenter;
+import com.posbilling.posbillingapplication.utility.Utility;
 
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
 
 import butterknife.BindView;
 import butterknife.OnClick;
@@ -37,15 +49,28 @@ import static com.posbilling.posbillingapplication.utility.Constants.CAMERA_IMAG
 import static com.posbilling.posbillingapplication.utility.Constants.CAMERA_PERMISSION_REQUEST_CODE;
 import static com.posbilling.posbillingapplication.utility.Constants.GALLERY_IMAGE_REQUEST_CODE;
 import static com.posbilling.posbillingapplication.utility.Constants.GALLERY_PERMISSION_REQUEST_CODE;
+import static com.posbilling.posbillingapplication.utility.Constants.languageCodeMarathi;
+import static com.posbilling.posbillingapplication.utility.DataState.stateId;
+import static com.posbilling.posbillingapplication.utility.DataState.stateNamesEnglish;
+import static com.posbilling.posbillingapplication.utility.DataState.stateNamesMarathi;
 
 /**
  * Created by Ankur Shinde on 26,February,2020
  */
-public class FragmentAddCustomer extends BaseFragment {
+public class FragmentAddCustomer extends BaseFragment implements OnStateListClick {
 
     private Context mContext;
     private File profileImageFile;
     private Uri imageUri;
+    private Dialog dialogState;
+    private EditText edt_search_state;
+    private ArrayList<StateModel> stateList = new ArrayList<>();
+    private ArrayList<StateModel> secondStateList = new ArrayList<>();
+    private ArrayList<StateModel> filteredStateList = new ArrayList<>();
+    private StateListAdapter stateListAdapter;
+    private OnStateListClick onStateListClick = this;
+    private String languageCode = "";
+    private boolean showdialogue = false;
 
     @BindView(R.id.imageview_profile)
     CircleImageView imageview_profile;
@@ -57,34 +82,35 @@ public class FragmentAddCustomer extends BaseFragment {
     EditText edittext_enterTalukaName;
     @BindView(R.id.edittext_enterMobileNumber)
     EditText edittext_enterMobileNumber;
+    @BindView(R.id.edittext_selectStateName)
+    EditText edittext_selectStateName;
+    @BindView(R.id.edittext_enterDistrictName)
+    EditText edittext_enterDistrictName;
 
     @OnClick(R.id.imageview_mic_mobile_number)
-    void imageview_mic_mobile_number(){
-        speechToTextFunc(mContext, edittext_enterMobileNumber,2);
+    void imageview_mic_mobile_number() {
+        speechToTextFunc(mContext, edittext_enterMobileNumber, 2);
     }
 
     @OnClick(R.id.imageview_mic_enter_name)
-    void imageview_mic_enter_name(){
-        speechToTextFunc(mContext, edittext_enterCustomerName,1);
+    void imageview_mic_enter_name() {
+        speechToTextFunc(mContext, edittext_enterCustomerName, 1);
     }
 
     @OnClick(R.id.imageview_mic_village_name)
-    void imageview_mic_village_name(){
-        speechToTextFunc(mContext, edittext_enterVillageName,1);
+    void imageview_mic_village_name() {
+        speechToTextFunc(mContext, edittext_enterVillageName, 1);
     }
 
     @OnClick(R.id.imageview_mic_taluka_name)
-    void imageview_mic_taluka_name(){
-        speechToTextFunc(mContext, edittext_enterTalukaName,1);
+    void imageview_mic_taluka_name() {
+        speechToTextFunc(mContext, edittext_enterTalukaName, 1);
     }
 
     @OnClick(R.id.imageview_mic_district_name)
-    void imageview_mic_district_name(){
-
+    void imageview_mic_district_name() {
+        speechToTextFunc(mContext, edittext_enterDistrictName, 1);
     }
-
-
-
 
     @OnClick(R.id.imageview_edit_pic)
     void imageview_edit_pic() {
@@ -112,8 +138,107 @@ public class FragmentAddCustomer extends BaseFragment {
         if (mContext == null) {
             mContext = ((ActivityDashboard) getContext());
         }
-        ((ActivityDashboard)getContext()).setTitleOfScreen(getString(R.string.menu_add_customer));
+        ((ActivityDashboard) getContext()).setTitleOfScreen(getString(R.string.menu_add_customer));
+        languageCode = Utility.getInstance().getLanguage(mContext);
+    }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+        showStateDialogue();
+    }
+
+    private void showStateDialogue() {
+        String tag = "";
+        edittext_selectStateName.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                final int DRAWABLE_LEFT = 0;
+                final int DRAWABLE_TOP = 1;
+                final int DRAWABLE_RIGHT = 2;
+                final int DRAWABLE_BOTTOM = 3;
+                if (event.getAction() == MotionEvent.ACTION_UP) {
+                    showdialogue = true;
+                    dialogState = new Dialog(getContext());
+                    if (!dialogState.isShowing()) {
+                        stateList.clear();
+
+                        for (int i = 0; i < stateId.length; i++) {
+                            StateModel stateModel = new StateModel();
+                            stateModel.setStateNameEnglish(stateNamesEnglish[i]);
+                            stateModel.setStateNameMarathi(stateNamesMarathi[i]);
+                            stateModel.setStateId(stateId[i]);
+                            stateList.add(stateModel);
+                        }
+                        dialogState.getWindow().addFlags(Window.FEATURE_NO_TITLE);
+                        dialogState.setCancelable(true);
+                        dialogState.setContentView(R.layout.custom_state_dialogue);
+                        edt_search_state = dialogState.findViewById(R.id.edt_search_state);
+                        secondStateList.clear();
+                        filteredStateList.clear();
+                        secondStateList.addAll(stateList);
+                        filteredStateList.addAll(stateList);
+                        RecyclerView recyclerView = dialogState.findViewById(R.id.recyclerview_contact);
+                        stateListAdapter = new StateListAdapter(mContext, stateList, onStateListClick, languageCode);
+                        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
+                        recyclerView.setLayoutManager(linearLayoutManager);
+                        recyclerView.setAdapter(stateListAdapter);
+                        setSearchFilter();
+                        dialogState.show();
+                    }
+                    return true;
+
+                }
+                return false;
+            }
+        });
+    }
+
+    private void setSearchFilter() {
+        edt_search_state.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                filter(s.toString());
+            }
+        });
+    }
+
+    private void filter(String text) {
+        filteredStateList.clear();
+        try {
+            for (StateModel item : secondStateList) {
+                if (item != null) {
+                    if (languageCode.equalsIgnoreCase(languageCodeMarathi)) {
+                        if (item.getStateNameMarathi().toLowerCase().contains(text.toLowerCase())) {
+                            filteredStateList.add(item);
+                        }
+                    } else {
+                        if (item.getStateNameEnglish().toLowerCase().contains(text.toLowerCase())) {
+                            filteredStateList.add(item);
+                        }
+                    }
+
+                }
+            }
+            if (text.trim().equals("")) {
+                filteredStateList.clear();
+                filteredStateList.addAll(secondStateList);
+                /*                filteredStateList.addAll(stateList);*/
+            }
+            stateListAdapter.filterList(filteredStateList);
+        } catch (Exception a) {
+
+        }
     }
 
     @Override
@@ -189,7 +314,6 @@ public class FragmentAddCustomer extends BaseFragment {
         Bitmap thumbnail = (Bitmap) data.getExtras().get("data");
         ByteArrayOutputStream bytes = new ByteArrayOutputStream();
         thumbnail.compress(Bitmap.CompressFormat.JPEG, 90, bytes);
-
         profileImageFile = new File(Environment.getExternalStorageDirectory(),
                 System.currentTimeMillis() + ".jpg");
         FileOutputStream fo;
@@ -286,27 +410,13 @@ public class FragmentAddCustomer extends BaseFragment {
         }
     }
 
-
-    /*////
     @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[]
-            grantResults) {
-        if (CAMERA_PERMISSION_REQUEST_CODE == requestCode) {
-            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                setError("Permission granted successfully");
-                Toast.makeText(getContext(), "Permission granted successfully", Toast.LENGTH_LONG).show();
-                cameraIntent();
-            } else {
-                PermissionUtils.setShouldShowStatus(getContext(), Manifest.permission.CAMERA);
-            }
-        } else if (GALLERY_PERMISSION_REQUEST_CODE == requestCode) {
-            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                setError("Permission granted successfully");
-                Toast.makeText(getContext(), "Permission granted successfully", Toast.LENGTH_LONG).show();
-                galleryIntent();
-            } else {
-                PermissionUtils.setShouldShowStatus(getContext(), Manifest.permission.READ_EXTERNAL_STORAGE);
-            }
+    public void onStateListClick(View view, int position) {
+        if (languageCode.equalsIgnoreCase(languageCodeMarathi)) {
+            edittext_selectStateName.setText(filteredStateList.get(position).getStateNameMarathi());
+        } else {
+            edittext_selectStateName.setText(filteredStateList.get(position).getStateNameEnglish());
         }
-    }*/
+        dialogState.dismiss();
+    }
 }
--- app/src/main/java/com/posbilling/posbillingapplication/activity/dashboard/ui/expenses/FragmentExpenses.java
+++ app/src/main/java/com/posbilling/posbillingapplication/activity/dashboard/ui/expenses/FragmentExpenses.java
@@ -1,35 +1,99 @@
 package com.posbilling.posbillingapplication.activity.dashboard.ui.expenses;
 
+import android.app.DatePickerDialog;
+import android.content.Context;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.DatePicker;
+import android.widget.EditText;
 
-import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
+import androidx.annotation.Nullable;
 
 import com.posbilling.posbillingapplication.R;
+import com.posbilling.posbillingapplication.activity.dashboard.ActivityDashboard;
+import com.posbilling.posbillingapplication.utility.BaseFragment;
+import com.posbilling.posbillingapplication.utility.BasePresenter;
 
-public class FragmentExpenses extends Fragment {
+import java.util.Calendar;
 
-    private ToolsViewModel toolsViewModel;
+import butterknife.BindView;
+import butterknife.OnClick;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        toolsViewModel =
-                ViewModelProviders.of(this).get(ToolsViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_expenses, container, false);
-        final TextView textView = root.findViewById(R.id.text_tools);
-        toolsViewModel.getText().observe(this, new Observer<String>() {
+public class FragmentExpenses extends BaseFragment {
+
+    private Context mContext;
+    final Calendar dob_currentDate = Calendar.getInstance();
+
+    @BindView(R.id.edittext_enterDate)
+    EditText edittext_enterDate;
+    @BindView(R.id.edittext_enterAmount)
+    EditText edittext_enterAmount;
+    @BindView(R.id.edittext_enterExpense)
+    EditText edittext_enterExpense;
+
+    @OnClick(R.id.imageview_mic_enter_expense)
+    void imageview_mic_enter_expense() {
+        speechToTextFunc(mContext,edittext_enterExpense,1);
+    }
+
+    @OnClick(R.id.imageview_mic_enter_amount)
+    void imageview_mic_enter_amount(){
+        speechToTextFunc(mContext,edittext_enterAmount,2);
+    }
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        this.mContext = context;
+        super.onAttach(context);
+    }
+
+    @Override
+    protected BasePresenter getPresenter() {
+        return null;
+    }
+
+    @Override
+    protected void setPresenter() {
+
+    }
+
+    @Override
+    protected int getLayout() {
+        return R.layout.fragment_expenses;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        if (mContext == null){
+            mContext = ((ActivityDashboard)getContext());
+        }
+        ((ActivityDashboard)getContext()).setTitleOfScreen(getString(R.string.menu_expenses));
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        setCalendar();
+    }
+
+    private void setCalendar() {
+        edittext_enterDate.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onClick(View v) {
+                DatePickerDialog datePickerDialog = new DatePickerDialog(mContext, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                        edittext_enterDate.setText(dayOfMonth+"/"+month+"/"+year);
+                    }
+                },dob_currentDate.get(dob_currentDate.YEAR), dob_currentDate.get(Calendar.MONTH), dob_currentDate.get(Calendar.DAY_OF_MONTH));
+                datePickerDialog.setTitle("Select Date");
+                datePickerDialog.getDatePicker().setMaxDate(System.currentTimeMillis());
+                datePickerDialog.show();
             }
         });
-        return root;
     }
+
+
 }
